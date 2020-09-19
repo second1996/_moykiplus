5c23a3aeba7b8faaedb5dbd033de52de9e441039
second1996 <vital10012@gmail.com>
2020-09-19 23:04:08 +0300

    Update

--- app/js/files/scripts.js
+++ app/js/files/scripts.js
@@ -1,3 +1,27 @@
 $('.dropdown-menu').click(function(e) {
-	e.stopPropagation();
-});
+	e.stopPropagation()
+})
+
+$('.header-bottom .navigation .search-form input').on('click', function() {
+	$(this).parents('.search-form').addClass('is-active')
+	$('.header-bottom .navigation').addClass('search-toggled')
+	$('.header-bottom .navigation .menu').addClass('is-hidden')
+	if( $('.modal-backdrop').length == 0 ) {
+		$('body').append('<div class="modal-backdrop fade"></div>')
+		$('.modal-backdrop').on('click', function() {
+			$(this).remove()
+			$('.header-bottom .navigation').removeClass('search-toggled')
+			$('.header-bottom .navigation .search-form').removeClass('is-active')
+			$('.header-bottom .navigation .menu').removeClass('is-hidden')
+		})
+	}
+})
+
+// $(document).on('click', function(e) {
+// 	var el = '.search-form';
+// 	if ( $(e.target).closest(el).length) {
+// 		console.log(true);
+// 	} else {
+// 		console.log(false);
+// 	}
+// })
