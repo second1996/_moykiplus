604b8c298413ad024b324d6636dc143d75bf3acc
second1996 <vital10012@gmail.com>
2020-09-24 00:23:11 +0300

    Update

--- app/js/files/sliders.js
+++ app/js/files/sliders.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
+
 	/**
 	 * Mega Menu Brands slider
 	 */
-	$('.mega-menu .brands-slider-container').each(function(index, element){
+	$('.mega-menu .brands-slider').each(function(index, element){
 		$(element).addClass('brands-slider-' + index)
-		var mmBrands = new Swiper('.brands-slider-' + index, {
+		var mmBrands = new Swiper('.brands-slider-' + index + ' .brands-slider-container', {
 			observer: true,
 			observeParents: true,
 			watchSlidesVisibility: true,
@@ -12,10 +13,28 @@ document.addEventListener("DOMContentLoaded", function() {
 			loop: false,
 			spaceBetween: 18,
 			navigation: {
-				nextEl: '.swiper-button-next',
-				prevEl: '.swiper-button-prev',
+				nextEl: '.brands-slider-' + index +' .swiper-button-next',
+				prevEl: '.brands-slider-' + index +' .swiper-button-prev',
 			},
 		})
+	})
+
+	/**
+	 * Home Banner slider
+	 */
+	var hBanner = new Swiper('.h-banner .h-banner-slider-container', {
+		slidesPerView:1,
+		loop: false,
+		spaceBetween: 10,
+		navigation: {
+			nextEl: '.h-banner-slider .swiper-button-next',
+			prevEl: '.h-banner-slider .swiper-button-prev',
+		},
+		pagination: {
+			el: '.h-banner-slider .swiper-pagination',
+			type: 'bullets',
+			clickable: true,
+		},
 	})
 
 })
