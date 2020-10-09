bf95c4b1bcaed3150ff475e96054c89b752901e8
second1996 <vital10012@gmail.com>
2020-10-09 14:50:18 +0300

    Update

--- app/js/files/scripts.js
+++ app/js/files/scripts.js
@@ -129,4 +129,32 @@ jQuery(document).ready( function($) {
 		$(this).parents('.stores').toggleClass('_toggled')
 	})
 
+
+	/**
+	 * Fix closing dropdown menu when selecting the text
+	 */
+	$('.dropdown-menu').on('click', function (e) {
+		e.stopPropagation()
+	})
+
+
+	/**
+	 * Show input phone mask when checkbox is cheked
+	 */
+	var switch_phone_mask_checkbox = $('.switch-input-mask').find('input[type="checkbox"]'),
+			switch_phone_mask_input = switch_phone_mask_checkbox.parents('.form-group').find('input[type="tel"]');
+
+	function switch_phone_mask() {
+		if( switch_phone_mask_checkbox.is(':checked') ) {
+			switch_phone_mask_input.mask('+7 (999) 999-99-99')
+		} else {
+			switch_phone_mask_input.mask('0#')
+		}
+	}
+	switch_phone_mask()
+
+	switch_phone_mask_checkbox.on('input checked', function() {
+		switch_phone_mask()
+	})
+
 })
